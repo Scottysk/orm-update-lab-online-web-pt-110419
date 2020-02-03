a586c80f46b57a009c90846e2cc631d2c242e55a
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-03 20:55:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       INSERT INTO students(name, grade)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+      DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   
