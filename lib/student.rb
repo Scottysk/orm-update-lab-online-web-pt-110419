5dc2b60b3ccf578ac4487118d151c3f179921602
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-03 20:53:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,17 @@ class Student
     sql = "DROP TABLE students"
       DB[:conn].execute(sql)
   end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end
 
   
 
