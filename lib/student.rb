2926bb4928603f417612c53da7bce68ae8c7245a
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-03 20:54:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,13 +29,7 @@ class Student
   
   def save
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-      )
-      SQL
-      DB[:conn].execute(sql)
+      INSERT INTO students
   end
 
   
